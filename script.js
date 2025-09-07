c6d4ee151df931edeb46c6c5686befa843c7262e
MTPlaysGes <esquadraoninja@gmail.com>
2025-09-06 23:48:45 -0300

    Atualização: site estilizado com leaderboard e seções

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Bem-vindo ao site da Crew!");
+// Lista de membros e infamy (exemplo)
+const members = [
+  { name: "MTPlays", infamy: 12000 },
+  { name: "Azazel", infamy: 9500 },
+  { name: "Yoru", infamy: 8700 },
+  { name: "Kira", infamy: 8200 },
+  { name: "Luna", infamy: 7800 }
+];
+
+const tbody = document.getElementById("leaderboard-body");
+
+members.forEach((member, index) => {
+  const tr = document.createElement("tr");
+  tr.innerHTML = `
+    <td>${index + 1}</td>
+    <td>${member.name}</td>
+    <td>${member.infamy}</td>
+  `;
+  tbody.appendChild(tr);
+});
